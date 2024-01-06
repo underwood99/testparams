a40d829468a5ec171d922fbf7b9977913b72a786
underwood <155793956+underwood99@users.noreply.github.com>
2024-01-06 06:54:04 +0000

    feat: Update log message, add homepage response and add new endpoint '/vlhu/testservice/go-tcp-greeter-5c6/v1.0'

--- httpserver.go
+++ httpserver.go
@@ -8,12 +8,17 @@ import (
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	digcloudlog.DCL_Info("got http request")
+	digcloudlog.DCL_Info("home got http request")
 	fmt.Fprintf(w, "Homepage")
+}
+func Home1(w http.ResponseWriter, r *http.Request) {
+	digcloudlog.DCL_Info("home1 got http request")
+	fmt.Fprintf(w, "Homepage1")
 }
 
 func httpStart() error {
 	http.HandleFunc("/", Home)
+	http.HandleFunc("/vlhu/testservice/go-tcp-greeter-5c6/v1.0", Home1)
 
 	// Spinning up the server.
 	err := http.ListenAndServe(":8080", nil)
