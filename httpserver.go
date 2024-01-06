0b851a5fb69425b2bf52f96e5edc617b2e49fa89
underwood <155793956+underwood99@users.noreply.github.com>
2024-01-06 06:46:38 +0000

    add http log

--- httpserver.go
+++ httpserver.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	digcloudlog "github.com/digautos-library/digCloudLogGo"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
+	digcloudlog.DCL_Info("got http request")
 	fmt.Fprintf(w, "Homepage")
 }
 
