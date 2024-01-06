942b138d6b0b2e7f3ce79c910d322b0a709ca194
underwood <155793956+underwood99@users.noreply.github.com>
2024-01-06 04:48:47 +0000

    Update 5 files

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	digcloudlog "github.com/digautos-library/digCloudLogGo"
 	"os"
 )
 
@@ -9,4 +10,13 @@ func main() {
 	serverAddress := os.Getenv("SERVER_ADDRESS")
 	fmt.Println("Hello, World!")
 	fmt.Println(serverAddress)
+	err := digcloudlog.DCL_addLogflare("d0399bff-7fc7-4874-a572-05309021d853", "WoMn49mFQDkh")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if serverAddress == "" {
+		serverAddress = "no params"
+	}
+	digcloudlog.DCL_Info(serverAddress)
 }
